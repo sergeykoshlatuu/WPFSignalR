457f841cee2db8a647c8820d0743f3036832ce79
sergeykoshlatuu <koshsy6363@gmail.com>
2019-04-08 18:41:26 +0300

    Hubs

---
+++ SignalRChatServer/WebApplication3/Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task Send(string user, string message)
+        {
+            await Clients.All.SendAsync("AddMessage", user, message);
+        }
+    }
+}
